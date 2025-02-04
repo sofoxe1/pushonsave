d9d57abc33defe246eea8a96bbac04e5ec6e4366
Kinse <22683778+sofoxe1@users.noreply.github.com>
2025-02-04 02:58:20 +0100

    yeah forgot abt that

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ fn main() -> Result<()> {
            Ok(_) => {
             println!("adding to repository");
             Command::new("git").args(["add","-A"]).output().expect("failed to add to repository");
-            Command::new("git").args(["commit","-m"]).output().expect("failed to commit to repository");
+            Command::new("git").args(["commit","-m","pushonsave"]).output().expect("failed to commit to repository");
             Command::new("git").arg("push").output().expect("failed to push to remote");// no way to spawn hundrends of threads if it requires password :3
            },
            Err(e) => println!("watch error: {:?}", e),
